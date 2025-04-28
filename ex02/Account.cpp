f1e46f3ccd46e9a5085d7b45f3eed2bf7ebeb2ce
Manuel Mazunga De Almeida <maalmeid@c1r1s3.42luanda.com>
2025-04-28 10:31:07 +0100

    Added Account.cpp

---
+++ ex02/Account.cpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Account.cpp                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maalmeid <maalmeid@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 10:21:45 by maalmeid          #+#    #+#             */
+/*   Updated: 2025/04/28 10:29:26 by maalmeid         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Account.hpp"
+#include <iostream>
+#include <iomanip>
+#include <ctime>
+
+int Account::_nbAccounts = 0;
+int Account::_totalAmount = 0;
+int Account::_totalNbDeposits = 0;
+int Account::_totalNbWithdrawals = 0;
+
+int Account
